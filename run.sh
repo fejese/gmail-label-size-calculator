e2edbfb424157e35b71b315f36a47bfeb9c08618
Endre Fejes <fejese@fejese.com>
2020-05-29 22:28:58 +0100

    initial commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+base_dir=$(dirname "$(realpath $0)")
+
+python_root=${base_dir}/.python
+
+PATH="${python_root}"/bin:$PATH PYTHONPATH="${python_root}" ${base_dir}/run.py
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python_root=$(dirname "$(realpath $0)")/.python
+
+pip3 install --target "${python_root}" --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
