9d591b41dc03420a5726d6c8a4740c1beced78e1
Endre Fejes <fejese@fejese.com>
2020-05-30 02:08:07 +0100

    this works

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ base_dir=$(dirname "$(realpath $0)")
 
 python_root=${base_dir}/.python
 
-PATH="${python_root}"/bin:$PATH PYTHONPATH="${python_root}" ${base_dir}/run.py
+PATH="${python_root}"/bin:$PATH PYTHONPATH="${python_root}" ${base_dir}/run.py "$@"
--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@
 
 python_root=$(dirname "$(realpath $0)")/.python
 
-pip3 install --target "${python_root}" --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+pip3 install --target "${python_root}" --upgrade \
+    google-api-python-client \
+    google-auth-httplib2 \
+    google-auth-oauthlib \
+    oauth2client
